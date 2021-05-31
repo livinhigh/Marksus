32c9a8eb4777a744dba6426c6c47360e68c43bbf
Jagathnath <52819237+Jagathnath@users.noreply.github.com>
2021-05-31 21:15:14 +0530

    Menu Done
    Encrypt section, and dictionary search left

--- main.py
+++ main.py
@@ -1,61 +1,43 @@
-import operator
-import re
-import collections
-
-charfrequency = {}
-charfrequencysorted = {}
-alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+from wordcrypt import wordshift
+from filecrypt import readfile
 
 
-fileObj = open("filename.txt", "r")
-charStream = fileObj.read()
-charStream = charStream.lower()
-charStream = re.sub("[^a-z]","",charStream,0, re.MULTILINE )
-print(charStream)
-for char in  charStream:
-    if char in charfrequency:
-         charfrequency[char] += 1
+def filecall():
+    print("File Path : ")
+    path = input()
+    readfile(path)
+
+def wordcall():
+    print("Encrypted Word : ")
+    cipher = input()
+    wordshift(cipher)
+
+
+def switch2():
+    print("1. Decrypt Textfile ")
+    print("2. Decrypt Word/Sentence")
+    choice2 = int(input())
+
+    if choice2 == 1:
+        filecall(),
+    elif choice2 == 2:
+        wordcall()
     else:
-        charfrequency[char] = 1
+        print("Oops wring choice")
+        switch2()
+
+def switch1():
+    print("1. Encrypt\n")
+    print("2. Decrypt\n")
+    choice = int(input())
+    if choice == 1:
+        print("under work"),
+    elif choice == 2:
+        switch2()
+    else:
+        print("Oops wrong choice")
+        switch1()
 
 
-def freqDictionary(charfrequency):
-    print(charfrequency)
-    total=0
-    for i in charfrequency:
-        total += charfrequency[i]
-    for j in charfrequency:
-        charfrequency[j] = (float)((charfrequency[j]/total)*100)
-    charfrequencysorted = dict(sorted(charfrequency.items(), key=operator.itemgetter(1), reverse=True))
-    return charfrequencysorted
-
-def decrypt(shift):
-    finput = open ("filename.txt", "r")
-    inputstr = finput.read()
-    outputstr = ''
-    for i in range(len(inputstr)):
-        char = inputstr[i]
-        if (char.isupper()):
-            outputstr += chr((ord(char) + shift-65) % 26 +65)
-        elif (char.isspace()):
-            outputstr += " "
-        elif (char.islower()):
-            outputstr += chr((ord(char) + shift -97) % 26 +97)
-    foutput = open("output.txt","a")
-    foutput.write("%s %s" % ("Shift :",shift))
-    foutput.write('\n')
-    foutput.write(outputstr)
-    foutput.write('\n')
-    return
-
-
-
-charfrequencysorted = freqDictionary(charfrequency)
-foutput = open("output.txt","r+")
-foutput.truncate(0)
-foutput.close()
-for i in range(0,5):
-        testchar = list(charfrequencysorted.keys())[i]
-        shift = alphabet.index('e') - alphabet.index(str(testchar))
-        print(shift)
-        decrypt(shift)
+print("MARKSUS\n")
+switch1()
